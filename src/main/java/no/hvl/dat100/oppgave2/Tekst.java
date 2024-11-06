1cd87151c7dc887545046436d00258e56fb2ae10
184947 <aslevidar@gmail.com>
2024-11-06 22:45:48 +0100

    oppgave2

--- src/main/java/no/hvl/dat100/oppgave2/Tekst.java
+++ src/main/java/no/hvl/dat100/oppgave2/Tekst.java
@@ -4,33 +4,49 @@ import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave1.*;
 
 public class Tekst extends Innlegg {
+	
+	private String tekst;
 
 	// TODO: objektvariable 
 	
-	public Tekst () {
+public Tekst () {
+		
 		
 	}
 	
 	public Tekst(int id, String bruker, String dato, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
+		
+		super(id, bruker, dato);
+		this.tekst = tekst;
+		
+		//throw new UnsupportedOperationException(TODO.constructor("Tekst"));
 	}
 	
+	
 	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
+		
+		super(id, bruker, dato, likes);
+		this.tekst = tekst;
+		
+		//throw new UnsupportedOperationException(TODO.constructor("Tekst"));
 	}
 	
 	public String getTekst() {
-		throw new UnsupportedOperationException(TODO.method());
+		return tekst;
+		
+		//throw new UnsupportedOperationException(TODO.method());
 
 	}
 
 	public void setTekst(String tekst) {
-		throw new UnsupportedOperationException(TODO.method());
+		this.tekst = tekst;
+		//throw new UnsupportedOperationException(TODO.method());
 	}
 
 	@Override
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		return "TEKST\n" + getId() + "\n" + getBruker() + "\n" + getDato() + "\n" + getLikes() + "\n" + tekst + "\n";
+    //throw new UnsupportedOperationException(TODO.method());
 
 	}
 	
